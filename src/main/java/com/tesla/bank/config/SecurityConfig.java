39835b91f45c399e021a4e1e62016789c3ea60c5
Ahmed Baz <ahmedbaz1024@gmail.com>
2025-01-29 14:22:11 +0200

    - configure database and liquibase - update SecurityConfig to use JdbcUserDetailsManager

--- src/main/java/com/tesla/bank/config/SecurityConfig.java
+++ src/main/java/com/tesla/bank/config/SecurityConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -33,22 +33,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails admin = User
-                .withUsername("admin@tesla.com")
-                .password("{bcrypt}$2a$12$F9s8wf9Jd0iKP4zW1mlxoOg7ptPc6E4qOEVpWVmxnhGcvH8.8ra2a")
-                .authorities("admin").build();
-        UserDetails maker = User
-                .withUsername("maker@tesla.com")
-                .password("{bcrypt}$2a$12$3CHj8.DAqOTqwbKM6Az/2e0Ej08sVXBaeWPVKsL45P3RCvxyXiI52")
-                .authorities("maker").build();
-        UserDetails checker = User
-                .withUsername("checker@tesla.com")
-                .password("{bcrypt}$2a$12$3CHj8.DAqOTqwbKM6Az/2e0Ej08sVXBaeWPVKsL45P3RCvxyXiI52")
-                .authorities("checker").build();
-
-        UserDetails user = User.withUsername("ahmedbaz").password("{noop}12345").authorities("read").build();
-        return new InMemoryUserDetailsManager(admin, maker, checker, user);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean

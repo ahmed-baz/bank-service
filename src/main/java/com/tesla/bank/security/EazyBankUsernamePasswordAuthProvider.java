b22b75f79e5e5dc74fa4987ceef6089d46e77291
Ahmed Baz <ahmedbaz1024@gmail.com>
2025-02-03 13:48:47 +0200

    create EazyBankUsernamePasswordAuthProvider

---
+++ src/main/java/com/tesla/bank/security/EazyBankUsernamePasswordAuthProvider.java
@@ -0,0 +1,33 @@
+package com.tesla.bank.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("!prod")
+@RequiredArgsConstructor
+public class EazyBankUsernamePasswordAuthProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        //TODO: do business logic like check user age, his country etc
+        return new UsernamePasswordAuthenticationToken(username, password, userDetails.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
